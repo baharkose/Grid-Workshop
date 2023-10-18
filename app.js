e7415cf385643e47b4ff484f5d23bdd18b666dd5
baharkose <baharkse17@gmail.com>
2023-10-18 13:13:15 +0300

    added onclick data.value

---
+++ app.js
@@ -0,0 +1,6 @@
+function changeDataStyles(){
+    var dataInput = document.querySelector(".data-input");
+    dataInput.style.backgroundColor = '#91ffeb74';
+}
+
+var equalButton = document.querySelector(".equal");
